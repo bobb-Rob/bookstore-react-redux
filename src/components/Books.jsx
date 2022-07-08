0b933a6087be179910c34e95dbbaa70e7cd668e7
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-08 11:02:15 +0100

    Add form for new book

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Book from './Book';
+import AddBookForm from './AddBookForm';
 
 const Books = () => (
   <div>
     <Book title="Rich Dad Poor Dad" author="Robert Kiyosaki" />
+    <AddBookForm />
   </div>
 );
 
