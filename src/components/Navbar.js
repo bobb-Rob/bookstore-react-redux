6c1649d86d546f7414bf9e1cdc73409f6309e46a
Robertson Arthur <robertsonakpan@gmail.com>
2022-07-14 19:29:28 +0100

    omplete navbar styling, install fontawesome-react dependencies

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import './styles/navbar.css';
 
 function Navbar({ name }) {
   return (
     <nav>
-      <span>{name}</span>
-      <div>
-        <Link to="/">BOOKS</Link>
-        {' '}
-        |
-        {' '}
-        <Link to="category">CATEGORY</Link>
+      <div
+        style={{ display: 'flex', alignItems: 'center' }}
+      >
+        <Link className="logo" to="/">{name}</Link>
+        <div>
+          <Link className="nav-links" to="/">BOOKS</Link>
+          <Link className="nav-links" to="category">CATEGORY</Link>
+        </div>
+      </div>
+      <div className="user-icon">
+        <FontAwesomeIcon icon={faUser} />
       </div>
     </nav>
   );
